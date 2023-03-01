2f057b9eb519038b7467f5794a5206d8c0ec7844
Sean <newnesid@gmail.com>
2023-02-28 23:25:24 -0700

    init

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -90,6 +90,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
+  return;
   block.textContent = '';
 
   // fetch nav content
